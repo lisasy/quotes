80d3d7647a699ab02d8c00c19eee81f8330a2130
Lisa Sy <lisasyis@gmail.com>
2014-12-08 11:10:46 -0600

    Create global namespace for app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,33 +1,7 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name quotesApp
- * @description
- * # quotesApp
- *
- * Main module of the application.
- */
-angular
-  .module('quotesApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var quotesApp = angular.module("quotesApp", [
+  'ngAnimate',
+  'ngResource',
+  'ngRoute',
+]);
