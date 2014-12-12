c47ab28e6e1526b912918852c3003b28f498007d
Lisa Sy <lisasyis@gmail.com>
2014-12-11 18:45:15 -0600

    Navigate through array of quotes

--- app/scripts/controllers/quotes.js
+++ app/scripts/controllers/quotes.js
@@ -12,5 +12,17 @@ quotesApp.controller("QuotesController", ["$scope", function($scope) {
       author: "Oprah Winfrey",
       content: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."
     }
-  ]
+  ];
+
+  $scope.quoteIndex = 0;
+  $scope.quote = {};
+
+  $scope.nextQuote = function() {
+    if ($scope.quoteIndex >= $scope.quotes.length-1) {
+      $scope.quoteIndex = 0;
+    } else {
+      $scope.quoteIndex++;
+    }
+    // console.log($scope.quotes.length + '/' $scope.quoteIndex);
+  };
 }]);
