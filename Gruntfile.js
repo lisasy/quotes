d0ef500a763abb67b087e9f9b329b07a55c01396
Lisa Sy <lisasyis@gmail.com>
2014-12-17 15:09:25 -0500

    Remove uglify for js

--- Gruntfile.js
+++ Gruntfile.js
@@ -254,15 +254,15 @@ module.exports = function (grunt) {
         }
       }
     },
-    uglify: {
-      dist: {
-        files: {
-          '<%= yeoman.dist %>/scripts/scripts.js': [
-            '<%= yeoman.dist %>/scripts/scripts.js'
-          ]
-        }
-      }
-    },
+    // uglify: {
+    //   dist: {
+    //     files: {
+    //       '<%= yeoman.dist %>/scripts/scripts.js': [
+    //         '<%= yeoman.dist %>/scripts/scripts.js'
+    //       ]
+    //     }
+    //   }
+    // },
     concat: {
       dist: {}
     },
