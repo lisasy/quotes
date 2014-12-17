ad4d88ad791257ff2bf4ab07b1a4fe7ab6db464a
Lisa Sy <lisasyis@gmail.com>
2014-12-17 14:47:12 -0500

    Fix things to enable server

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
-var logger = require('morgan');
- 
-app.use(logger());
+
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
