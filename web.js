f269b0ff87580a8c81999b9b81b649d693e5cb34
Lisa Sy <lisasyis@gmail.com>
2014-12-17 14:41:03 -0500

    Modify type in web js file

--- web.js
+++ web.js
@@ -3,6 +3,6 @@ var express = require('express');
 var app = express();
 var logger = require('morgan');
  
-app.use(logger);
+app.use(logger());
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
